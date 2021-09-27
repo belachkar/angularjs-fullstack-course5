99232c9ba7f033c42b7e69c68c4c3204f982adfc
belachkar <belachkar_ali@hotmail.com>
2021-09-28 00:00:12 +0100

    Filters

--- examples/Lecture12/beginning-of-lecture-code/app.js
+++ examples/Lecture12/beginning-of-lecture-code/app.js
@@ -1,21 +1,24 @@
 (function () {
   'use strict';
 
-  angular.module('MsgApp', [])
-    .controller('MsgController', MsgController);
+  angular.module('MsgApp', []).controller('MsgController', MsgController);
 
-  MsgController.$inject = ['$scope'];
-  function MsgController($scope) {
+  MsgController.$inject = ['$scope', '$filter'];
+
+  function MsgController($scope, $filter) {
     $scope.name = 'Yaakov';
     $scope.stateOfBeing = 'hungry';
+    $scope.cookieCost = .45;
 
     $scope.sayMessage = function () {
-      return 'Yaakov likes to eat healthy snacks at night!';
+      let msg = 'Yaakov likes to eat healthy snacks at night!';
+      const output = $filter('uppercase')(msg);
+
+      return output;
     };
 
     $scope.feedYaakov = function () {
       $scope.stateOfBeing = 'fed';
     };
   }
-
 })();
