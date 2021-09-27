c78adf4b3f20cd8310acd5b2f0fcf59075134aef
belachkar <belachkar_ali@hotmail.com>
2021-09-28 00:32:13 +0100

    Custom Filters

--- examples/Lecture13/beginning-of-lecture-code/part2/app.js
+++ examples/Lecture13/beginning-of-lecture-code/part2/app.js
@@ -1,21 +1,28 @@
 (function () {
   'use strict';
 
-  angular.module('MsgApp', [])
-    .controller('MsgController', MsgController);
+  angular.module('MsgApp', []).controller('MsgController', MsgController).filter('loves', LovesFilter);
 
-  MsgController.$inject = ['$scope'];
-  function MsgController($scope) {
+  MsgController.$inject = ['$scope', 'lovesFilter'];
+
+  function MsgController($scope, lovesFilter) {
+    const msg = 'Yaakov likes to eat healthy snacks at night!';
+
     $scope.stateOfBeing = 'hungry';
 
-    $scope.sayMessage = function () {
-      var msg = 'Yaakov likes to eat healthy snacks at night!';
-      return msg;
-    };
+    $scope.sayMessage = () => msg;
+    $scope.sayLovesMessage = () => lovesFilter(msg);
 
     $scope.feedYaakov = function () {
       $scope.stateOfBeing = 'fed';
     };
   }
 
+  function LovesFilter() {
+    return function (input) {
+      input = input || '';
+
+      return input.replace('likes', 'loves');
+    };
+  }
 })();
