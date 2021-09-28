2147b6eb63b7510d75c2b52aefffd612eca4556d
belachkar <belachkar_ali@hotmail.com>
2021-09-28 02:11:08 +0100

    Digest Cycle: watchers

--- examples/Lecture14/beginning-of-lecture-code/app.js
+++ examples/Lecture14/beginning-of-lecture-code/app.js
@@ -1,16 +1,34 @@
 (function () {
   'use strict';
 
-  angular.module('CounterApp', [])
-    .controller('CounterController', CounterController);
-
+  angular.module('CounterApp', []).controller('CounterController', CounterController);
   CounterController.$inject = ['$scope'];
+
   function CounterController($scope) {
+    $scope.onceCounter = 0;
+    $scope.counter = 0;
 
     $scope.showNumberOfWatchers = function () {
-
+      console.log('# of watcheres:', $scope.$$watchersCount);
     };
 
-  }
+    $scope.countOnce = () => {
+      $scope.onceCounter = 1;
+    };
 
+    $scope.upCount = () => {
+      $scope.counter++;
+    };
+
+    // Manule watchers
+    $scope.$watch('onceCounter', (newValue, oldValue) => {
+      console.log('onceCounter old Value:', oldValue);
+      console.log('onceCounter new Value:', newValue);
+    });
+
+    $scope.$watch('counter', (newValue, oldValue) => {
+      console.log('counter old Value:', oldValue);
+      console.log('counter new Value:', newValue);
+    });
+  }
 })();
