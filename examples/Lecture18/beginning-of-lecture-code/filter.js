868aa5a615c0e14df0368514c3ec697598387769
belachkar <belachkar_ali@hotmail.com>
2021-09-29 01:28:47 +0100

    Lecture 18: Filtered ng-repeat

--- examples/Lecture18/beginning-of-lecture-code/filter.js
+++ examples/Lecture18/beginning-of-lecture-code/filter.js
@@ -1,2 +1,5 @@
-var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log('Number array: ', numberArray);
+
+const filteredNbrArr = numberArray.filter((n) => n > 5);
+console.log(filteredNbrArr);
